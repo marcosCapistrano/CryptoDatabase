467382867d92ce88c1831d11b58abb5e3bb823cb
Marcos Capistrano <mcapistrano50@gmail.com>
2021-10-27 09:56:56 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,98 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+exports.__esModule = true;
+var axios_1 = require("axios");
+var ID_MAP_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/map";
+var QUOTES_LATEST_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest";
+var headers = {
+    "X-CMC_PRO_API_KEY": "bf638232-518c-4217-8806-8788e792109a"
+};
+// const response = Axios.get(`${QUOTES_LATEST_URL}?id=1`, {headers}).then(response => console.log(response.data.data['1'].quote))
+var client_1 = require("@prisma/client");
+var prisma = new client_1.PrismaClient();
+function main() {
+    return __awaiter(this, void 0, void 0, function () {
+        var response, allCryptos, _i, allCryptos_1, crypto_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: 
+                // Connect the client
+                return [4 /*yield*/, prisma.$connect()];
+                case 1:
+                    // Connect the client
+                    _a.sent();
+                    return [4 /*yield*/, axios_1["default"].get(ID_MAP_URL, { headers: headers })];
+                case 2:
+                    response = _a.sent();
+                    allCryptos = response.data.data;
+                    _i = 0, allCryptos_1 = allCryptos;
+                    _a.label = 3;
+                case 3:
+                    if (!(_i < allCryptos_1.length)) return [3 /*break*/, 6];
+                    crypto_1 = allCryptos_1[_i];
+                    console.log("Trying to create: ", crypto_1.name);
+                    return [4 /*yield*/, prisma.cryptocurrency.create({
+                            data: {
+                                id: crypto_1.id,
+                                name: crypto_1.name,
+                                symbol: crypto_1.symbol
+                            }
+                        })];
+                case 4:
+                    _a.sent();
+                    _a.label = 5;
+                case 5:
+                    _i++;
+                    return [3 /*break*/, 3];
+                case 6: return [2 /*return*/];
+            }
+        });
+    });
+}
+main()["catch"](function (e) {
+    throw e;
+})["finally"](function () { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, prisma.$disconnect()];
+            case 1:
+                _a.sent();
+                return [2 /*return*/];
+        }
+    });
+}); });
